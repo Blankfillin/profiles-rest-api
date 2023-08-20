16d1a598095adf839cdb5af3713f9b9bf25e76f3
Chun Lu <chun.lu.dev@gmail.com>
2023-08-20 09:09:35 -0700

    added logging

--- core/profiles_project/settings/templates/settings.dev.py
+++ core/profiles_project/settings/templates/settings.dev.py
@@ -3,3 +3,11 @@ SECRET_KEY = 'django-insecure-a4x%g3e71y$-djx=%2)k10w!&ge4bit^cg)e7qhohc9hs2g&dl
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+LOGGING['formatters']['colored'] = {  # type: ignore
+    '()': 'colorlog.ColoredFormatter',
+    'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
+}
+LOGGING['loggers']['cooking_core']['level'] = 'DEBUG'  # type: ignore
+LOGGING['handlers']['console']['level'] = 'DEBUG'  # type: ignore
+LOGGING['handlers']['console']['formatter'] = 'colored'  # type: ignore
