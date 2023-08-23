816256a51102abd6d67123221a048e06eb0d523c
Chun Lu <chun.lu.dev@gmail.com>
2023-08-22 20:25:13 -0700

    Added user profile feed endpoint

--- profiles_rest_api_core/profiles_api/serializers.py
+++ profiles_rest_api_core/profiles_api/serializers.py
@@ -35,3 +35,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
             instance.set_password(password)
 
         return super().update(instance, validated_data)
+
+
+class ProfileFeedSerializer(serializers.ModelSerializer):
+    """Serializes a profile feed object"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ("id", "user_profile", "status_text", "created_on")
+        extra_kwargs = {"user_profile": {"read_only": True}}
