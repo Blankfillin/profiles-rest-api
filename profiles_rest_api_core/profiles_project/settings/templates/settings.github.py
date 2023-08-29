a74843bad1bdd60e874d41d975a4ecfa2252f423
Blankfillin <114777854+Blankfillin@users.noreply.github.com>
2023-08-28 23:03:13 -0700

    testing workflow (#1)
    * testing workflow
    
    * corrected typo
    
    * split into 2 commands
    
    * change python version
    
    * corrected whitespace
    
    * added init.py
    
    * added pythonpath
    
    * using echo to set PYTHONPATH
    
    * testing with .
    
    * removed comment
    
    ---------
    
    Co-authored-by: Chun Lu <chun.lu.dev@gmail.com>

--- profiles_rest_api_core/profiles_project/settings/templates/settings.github.py
+++ profiles_rest_api_core/profiles_project/settings/templates/settings.github.py
@@ -1,5 +1,5 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@rk)kd1=fe^t*fwwb8u^_sp^y3gugh=)tkpj93ahiq25^!l9&"
+SECRET_KEY = "django-insecure-@rk)kd1=fe^t*fwwb8u^_sp^y3gugh=)tkpj93ahiq25^!l9!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
